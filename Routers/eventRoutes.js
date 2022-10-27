83c938780e69e7308e9cbf1e21c47582437ce315
Avri-Here <yoti1492@gmail.com>
2022-10-27 18:20:54 +0300

    Soo

--- Routers/eventRoutes.js
+++ Routers/eventRoutes.js
@@ -15,7 +15,7 @@ const obj = {
         console.log(newEvent);
         try {
             let response = await newEvent.save();
-            res.status(200).json({ data: response })
+            res.status(200).json({ status: "Good!", data: response })
 
 
         } catch (error) {
@@ -27,7 +27,6 @@ const obj = {
     }), allEvents: (async (req, res) => {
         try {
             const response = await Event.find({});
-            const filterArr = [];
             console.log(response);
             response.forEach((element, index) => {
                 response[index].Date = element.Date.slice(0, 10);
@@ -40,9 +39,7 @@ const obj = {
     }), EventChat: ((req, res) => {
         const { IdEvent, from, messages } = req.body;
         EventChat.find({ IdEvent: IdEvent }).then((response) => {
-            console.log(response);
             if (response.length === 0 && messages) {
-
                 const newEventChat = new EventChat({
                     IdEvent: IdEvent,
                     EventMessages: [{ from: from, messages: messages }]
@@ -72,17 +69,18 @@ const obj = {
             }
 
         })
+    }), EventByName: ((req, res) => {
+        const { IdEvent } = req.body;
+        Event.find({ NameEvent: IdEvent }).then((response) => {
 
+            return res.status(200).json(response)
 
-    }), Updateusers: () => {
-
-    }, deleteUser: () => {
-
-    }
+        })
+    })
 };
 router.get('/allEvents', obj.allEvents);
 router.post('/CreateEvent', obj.CreateEvent);
 router.post('/EventChat', obj.EventChat);
-// router.post('/delete', obj.deleteUser);
+// router.post('/EventsByName', obj.EventByName);
 // router.post('/Update', obj.Updateusers);
 module.exports = router;
