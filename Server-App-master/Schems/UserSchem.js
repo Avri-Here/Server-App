0781b9114d15e7a9e330140a22c6e0d0a7925361
7648597 <chaimbinder31@gmail.com>
2022-11-02 19:38:27 +0200

    עכשיו

--- Server-App-master/Schems/UserSchem.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const UserSchem = mongoose.Schema({
-    Name: { type: String, required: true },
-    password: { type: String, required: true },
-    photoUser: { type: String }
-})
-
-module.exports = mongoose.model('Users', UserSchem);
