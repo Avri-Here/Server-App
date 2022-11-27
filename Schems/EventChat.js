a576d1a05fa92c1b27d7f25a90e698dd886656da
7648597 <chaimbinder31@gmail.com>
2022-11-27 12:20:09 +0200

    אבי יוט תיקון

--- Schems/EventChat.js
+++ Schems/EventChat.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
 const EventChat = mongoose.Schema({
-    IdEvent: String,
-    EventMessages: [{ from: String, messages: String, timeMass: { type: Date, default: Date.now } }]
-})
+  IdEvent: String,
+  EventMessages: [
+    {
+      from: String,
+      messages: String,
+      timeMass: {
+        type: Date,
+        default: Date.now + 3 * 60 * 60 * 1000,
+      },
+    },
+  ],
+});
 
-module.exports = mongoose.model('EventChat', EventChat);
+module.exports = mongoose.model("EventChat", EventChat);
